668fed1732ab31497b72981c371940a8ab40df98
Sergei Gordeev <sergei.gordeev@soft-control.ru>
2026-07-30 12:14:45 +0300

    fix config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
+import * as path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   test: {
     globals: true,
